48a2fd13d8ebed90fe666582d372daa77b04d6cb
russoandre <andreprusso7@gmail.com>
2022-08-29 22:03:39 -0300

    Criação da sidebar
    Criei um componente para a side bar, utilizei css module para estilizar esse componente

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,27 @@
 import { Header } from "./components/Header";
 import './global.css';
-
+import styles from "./App.module.css";
+import { Post } from './Post.jsx';
+import { Sidebar } from "./components/Sidebar";
 
 export function App() {
   return (
     <div>
       <Header />
-      <h1>Jesse2</h1>
+      
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+            <Post
+              author="Diego Fernandes"
+              content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maxime itaque quas corporis beatae veritatis, reprehenderit asperiores vitae quod possimus qui dignissimos unde deleniti consequatur quae, repellat debitis sunt, est rerum!"
+          />
+          <Post
+            author="Gabriel Buzzi"
+            content="Um novo post muito legal"
+          />
+        </main>
+      </div>
     </div>
   )
 }
