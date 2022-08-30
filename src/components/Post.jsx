fa6d855ed94f20fa1e27fc55d34639f2c8dda84e
russoandre <andreprusso7@gmail.com>
2022-08-29 23:10:03 -0300

    Estilização Post e correção da Sidebar

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,30 +1,30 @@
 import styles from './Post.module.css';
 
+export function Post() {
+  return (
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <img className={styles.avatar} src="https://avatars.githubusercontent.com/u/101806622?v=4" />
+          <div className={styles.authorInfo}>
+            <strong>Danilo Sousa</strong>
+            <span>Jovem Aprendiz</span>
+          </div>
+        </div>
 
-export function Post(){
-    return(
-        <article className={styles.Post}>
-            <header>
-                <div className={styles.author}>
-                    <img className={styles.avatar} 
-                    src="https://avatars.githubusercontent.com/u/101806622?v=4"
-                    />
-                    <div className={styles.authorInfo}>
-                        <strong>Danilo Gonçalves</strong>
-                        <span>Jovem Aprendiz</span>
-                    </div>
-                </div>
-                <time title='29 de agosto às 22:22' dateTime='2022/08/29 22:22:00'>Publicado há 1 hora</time>
+        <time title="11 de Maio às 08:13h" dateTime="2022-05-11 08:13:00">Publicado há 1h</time>
+      </header>
 
-            </header>
-
-            <div className={styles.content}>
-            <p>Fala galeraa 👋</p>
-            <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
-            <p>👉 <a href='#'>jane.design/doctorcare</a></p>
-            <p><a href='#'>#novoprojeto #nlw #rocketseat </a></p>
-            </div>
-            
-        </article>
-    );
+      <div className={styles.content}>
+        <p>Fala galeraa 👋</p>
+        <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
+        <p><a href="">jane.design/doctorcare</a></p>
+        <p>
+          <a href="">#novoprojeto</a>{' '}
+          <a href="">#nlw</a>{' '}
+          <a href="">#rocketseat</a>
+        </p>
+      </div>
+    </article>
+  )
 }
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,29 +1,28 @@
+import { PencilLine } from 'phosphor-react'
+
 import styles from './Sidebar.module.css';
-import {PencilLine} from "phosphor-react";
 
+export function Sidebar() {
+  return (
+    <aside className={styles.sidebar}>
+      <img
+        className={styles.cover}
+        src="https://images.unsplash.com/photo-1544198365-f5d60b6d8190?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=100"
+      />
 
-export function Sidebar(){
-    return(
-      <aside className={styles.sidebar}>
-        <img className={styles.cover} 
-        src="https://images.unsplash.com/photo-1544198365-f5d60b6d8190?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50"
-        />
-        
-        <div className={styles.profile}>
-            <img className={styles.avatar} 
-            src="https://avatars.githubusercontent.com/u/51973239?v=4" 
-            />
+      <div className={styles.profile}>
+        <img className={styles.avatar} src="https://avatars.githubusercontent.com/u/51973239?v=4" />
 
-            <strong>André Russo</strong>
-            <span>Support Analyst - Intern</span>
-        </div>
-        <footer>
-            <a href="#">
-                <PencilLine size={20} />
-                Editar seu perfil</a>
-        </footer>
+        <strong>André Russo</strong>
+        <span>Support Analyst - Intern</span>
+      </div>
 
-      </aside>
-
-    );
+      <footer>
+        <a href="#">
+          <PencilLine size={20} />
+          Editar seu perfil
+        </a>
+      </footer>
+    </aside>
+  );
 }
